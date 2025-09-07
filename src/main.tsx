476f782471e7a7552049e1cf8febd54278f1ab0b
nothiaki <celsohenrique367@gmail.com>
2025-09-07 19:02:21 -0300

    fix: tailwind import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import Home from './pages/home.tsx';
 
 createRoot(document.getElementById('root')!).render(
